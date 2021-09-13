690021818292f2183eae86549ecbf75e860631d6
Zamzam Multazam <zamzaminvestor@gmail.com>
2021-09-13 22:32:25 +0900

    day 4

--- Data processing/data_processing.py
+++ Data processing/data_processing.py
@@ -7,26 +7,37 @@ from sklearn.impute import SimpleImputer
 
 #------data gathering
 #------importing dataset
-dataset = pd.read_csv('example_data.csv')
-
+filename = 'example_data.csv'
+dataset = pd.read_csv(filename)
+print(dataset)
 #------data exploring
 #------selecting column (features, target, etc)
 X = dataset.iloc[:, :-1].values #select all data except -1 = the last data or column
-Y = dataset.iloc[:, -1]
+Y = dataset.iloc[:, -1].values #target
+#------column name
+columns = dataset.columns.tolist()
+features = columns[:-1]
+target = columns[-1:]
 
 #------split the dataese into train dataset and test dataset
 X_train, X_test, Y_train, Y_test = train_test_split(X,Y) #import another file, if the testing data in different file
 
 #------Data cleansing: handling the missing data, suspicious data and errorness data
 #------missing data? using SimpleImputer
-imputer = SimpleImputer(missing_values=np.nan, strategy = 'mean') #for all nan data will be replaced by the mean
-imputer.fit(X[:, 1:3]) #will look all missing values only in the int column
-X[:, 1:3] = imputer.transform(X[:, 1:3])
-a=1
+#imputer = SimpleImputer(missing_values=np.nan, strategy = 'mean') #for all nan data will be replaced by the mean
+#imputer.fit(X[:, 1:3]) #will look all missing values only in the int column
+#X[:, 1:3] = imputer.transform(X[:, 1:3])
+missing_values = ['n/a', 'na', '--']
+dataset = pd.read_csv(filename, na_values = missing_values)
+#for i in range(dataset.shape[1]):
+    #for j in range(dataset.shape[0]):
 
-if type(a) is str:
-    print('str')
-else:
-    print('eror')
+
+
+
+
+
+
+#print(dataset.isnull().sum())
 
 
--- trial.py
+++ trial.py
@@ -6,21 +6,11 @@ from sklearn.preprocessing import LabelEncoder
 import pandas as pd
 le = LabelEncoder()
 from sklearn.datasets import load_iris
-x1=([[1,7,3],
-            [2,8,1],
-            [3,9,5],
-             [4,10,2]])
-c=['x1','x2','x3']
-x1=pd.DataFrame(x1, columns=c)
 
-y1=np.array([1,2,3,4])
-y2=np.array([[1],
-             [2],
-             [3],
-             [4]])
-y2=pd.DataFrame(y2, columns=['target'])
-y3=np.transpose(y2)
-
-print(drop_feature(x1,y2))
-
-
+df = pd.DataFrame({'A': ['a', 'b', 'a'], 'B': ['b', 'a', 'c'],'C': [1, 2, 3]})
+print(df)
+columns = df.columns
+features = columns.tolist()
+features1=['A', 'B', 'C']
+print(features[0])
+print(features1)
