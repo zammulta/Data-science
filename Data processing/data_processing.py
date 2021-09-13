f973f914eaf7b8cb54dee18276f25141c0c287ee
Zamzam Multazam <zamzaminvestor@gmail.com>
2021-09-13 23:56:47 +0900

    day 4

--- Data processing/data_processing.py
+++ Data processing/data_processing.py
@@ -9,35 +9,34 @@ from sklearn.impute import SimpleImputer
 #------importing dataset
 filename = 'example_data.csv'
 dataset = pd.read_csv(filename)
-print(dataset)
+
+
 #------data exploring
-#------selecting column (features, target, etc)
-X = dataset.iloc[:, :-1].values #select all data except -1 = the last data or column
-Y = dataset.iloc[:, -1].values #target
 #------column name
 columns = dataset.columns.tolist()
 features = columns[:-1]
 target = columns[-1:]
-
-#------split the dataese into train dataset and test dataset
-X_train, X_test, Y_train, Y_test = train_test_split(X,Y) #import another file, if the testing data in different file
+#------handling missing data
+missing_values = ['n/a', 'na', '--']
+for i in range(dataset.shape[1]):
+    for j in range(np.shape(missing_values)[0]):
+        dataset[columns[i]] = dataset[columns[i]].replace(missing_values[j], np.NaN)
+print(dataset.isnull().sum())
+print(dataset)
 
 #------Data cleansing: handling the missing data, suspicious data and errorness data
 #------missing data? using SimpleImputer
-#imputer = SimpleImputer(missing_values=np.nan, strategy = 'mean') #for all nan data will be replaced by the mean
-#imputer.fit(X[:, 1:3]) #will look all missing values only in the int column
-#X[:, 1:3] = imputer.transform(X[:, 1:3])
-missing_values = ['n/a', 'na', '--']
-dataset = pd.read_csv(filename, na_values = missing_values)
-#for i in range(dataset.shape[1]):
-    #for j in range(dataset.shape[0]):
+imputer = SimpleImputer(missing_values=np.nan, strategy = 'most_frequent') #most_requent = for all types data, mean = all except str
+imputer.fit(dataset) #will look all missing values only in the int column
+dataset = imputer.transform(dataset)
+
+#------selecting column (features, target, etc)
+X = dataset[:, :-1] #select all data except -1 = the last data or column
+Y = dataset[:, -1:] #target
+#------split the dataese into train dataset and test dataset
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y) #import another file, if the testing data in different file
 
 
 
-
-
-
-
-#print(dataset.isnull().sum())
 
 
